84ec907e6f98c54524a90a65455a1b663a134330
gardenofwine <gardenofwine@gmail.com>
2012-11-22 12:41:36 +0200

    minor fix agenda list view

--- src/web/www/javascripts/views/AgendaListView.js
+++ src/web/www/javascripts/views/AgendaListView.js
@@ -1,7 +1,7 @@
 OKnesset.app.views.AgendaListView = new Ext.extend(Ext.List, {
     id: 'AgendaListView',
     store: OKnesset.AgendaListStore,
-    itemTpl: '<div>{name}<span class="btn-mini-text">{public_owner_name}</span></div>',
+    itemTpl: '<div>{name}<span class="btn-mini-text">' + OKnesset.strings.by + ' {public_owner_name}</span></div>',
     onItemDisclosure: true
 });
 
