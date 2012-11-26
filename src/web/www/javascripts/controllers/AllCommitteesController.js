92de6889191eb0cf4487e092184a5096fe057776
Yoni Jah <yonjah@non>
2012-11-26 21:06:42 +0200

    syntex and style fix

--- src/web/www/javascripts/controllers/AllCommitteesController.js
+++ src/web/www/javascripts/controllers/AllCommitteesController.js
@@ -1,36 +1,36 @@
 Ext.regController('AllCommittees', {
 
 	Index: function(options) {
-        if ( ! this.AllCommitteesView)
-        {
-            this.AllCommitteesView = this.render({
-                xtype: 'AllCommitteesView',
-            });
+		if ( ! this.AllCommitteesView)
+		{
+			this.AllCommitteesView = this.render({
+				xtype: 'AllCommitteesView'
+			});
 
-        this.AllCommitteesView.addListener('itemtap',
-            	function(that, index, item, e) {
+		this.AllCommitteesView.addListener('itemtap',
+				function(that, index, item, e) {
 					var record = that.store.getAt(index);
 					OKnesset.app.controllers.navigation.dispatchPanel('CommitteeDetails/Index/' + record.data.id, options.historyUrl);
 				});
-        }
+		}
 
-        getAPIData({
-            apiKey : "committees",
-            success : function(data){
-		    	OKnesset.AllCommitteesStore.loadData(data);
-            },
-            failure: function(){
-                console.log("Failure loading committees json from server");
-            }
-        })
+		getAPIData({
+			apiKey : "committees",
+			success : function(data){
+				OKnesset.AllCommitteesStore.loadData(data);
+			},
+			failure:function(result){
+				OKnesset.onError('SERVER', ["Failure loading committees json from server", result]);
+			}
+		});
 
-        // don't track if the panal was reached by pressing 'back'
-        if (options.pushed){
-            GATrackPage('AllCommitteesView', '');
-        }
+		// don't track if the panal was reached by pressing 'back'
+		if (options.pushed){
+			GATrackPage('AllCommitteesView', '');
+		}
 
 
-        this.application.viewport.query('#toolbar')[0].setTitle(OKnesset.strings.committees);
-        this.application.viewport.setActiveItem(this.AllCommitteesView, options.animation);
-    }
+		this.application.viewport.query('#toolbar')[0].setTitle(OKnesset.strings.committees);
+		this.application.viewport.setActiveItem(this.AllCommitteesView, options.animation);
+	}
 });
--- src/web/www/javascripts/controllers/BillDetailsController.js
+++ src/web/www/javascripts/controllers/BillDetailsController.js
@@ -1,152 +1,151 @@
 Ext.regController('BillDetails', {
 
-    // index action
-    Index: function (options) {
+	// index action
+	Index: function (options) {
 
-         if (!this.BillDetailsView) {
-            this.BillDetailsView = this.render({
-                xtype: 'BillDetailsView',
-            });
+		var BillDetailsController = this,
+			billMakersList = this.BillDetailsView.query('#billMakersList')[0],
+			billContent = this.BillDetailsView.query('#billContent')[0],
+			billStage = this.BillDetailsView.query('#billStage')[0],
+			hideWhileLoading = [billStage, billContent, billMakersList],
+			bill;
+		if (!this.BillDetailsView) {
+			this.BillDetailsView = this.render({
+				xtype: 'BillDetailsView'
+			});
 
-            var BillDetailsController = this;
-            var billMakersList = this.BillDetailsView.query('#billMakersList')[0];
-            billMakersList.addListener('itemtap', function(that, index, item, e){
-                var record = that.store.getAt(index);
-                BillDetailsController._gotoMember(record);
-            });
-        }
+			billMakersList.addListener('itemtap', function(that, index, item, e){
+				var record = that.store.getAt(index);
+				BillDetailsController._gotoMember(record);
+			});
+		}
 
-        var BillDetailsController = this;
-        var billContent = this.BillDetailsView.query('#billContent')[0];
-        var billMakersList = this.BillDetailsView.query('#billMakersList')[0];
-        var billStage = this.BillDetailsView.query('#billStage')[0];
+		bill = getAPIData({
+			apiKey:'bill',
+			urlOptions : options.id,
+			success:function(data){
+				BillDetailsController.updateData(data);
+				BillDetailsController._refresh(hideWhileLoading);
+			},
+			failure : function(result){
+				console.log("Error receiving bill data. ", result);
+			}
+		});
 
-        var hideWhileLoading = [billStage, billContent, billMakersList];
+		// don't track if the panal was reached by pressing 'back'
+		if (options.pushed){
+			GATrackPage('BillDetailsView', options.id);
+		}
 
-        var bill = getAPIData({
-            apiKey:'bill',
-            urlOptions : options.id,
-            success:function(data){
-                BillDetailsController.updateData(data);
-                BillDetailsController._refresh(hideWhileLoading);                
-            },
-            failure : function(result){
-                console.log("Error receiving bill data. ", result);
-            }
-        });
-
-        // don't track if the panal was reached by pressing 'back'
-        if (options.pushed){
-            GATrackPage('BillDetailsView', options.id);
-        }        
-
-        if (!bill){
-            BillDetailsController._init(hideWhileLoading);
-        }
+		if (!bill){
+			BillDetailsController._init(hideWhileLoading);
+		}
 
 
-        this.application.viewport.setActiveItem(this.BillDetailsView, options.animation);
-        this.application.viewport.query('#toolbar')[0].setTitle(OKnesset.strings.billDetails);
+		this.application.viewport.setActiveItem(this.BillDetailsView, options.animation);
+		this.application.viewport.query('#toolbar')[0].setTitle(OKnesset.strings.billDetails);
 
-        if (BillDetailsController.BillDetailsView.scroller) {
-            BillDetailsController.BillDetailsView.scroller.scrollTo({
-                x: 0,
-                y: 0
-            });
-        }
-    },
-
-
-    _init: function(elementsToHIDE){
-
-        this.BillDetailsView.query('#billTitle')[0].update({
-            title: OKnesset.strings.LoadingPlsWait
-        });
-
-        elementsToHIDE.forEach(function(e){
-            e.hide();
-        });
-    },
-    _refresh: function(elementsToSHOW){
-
-        elementsToSHOW.forEach(function(e){
-            e.show();
-        });
-    },
-
-    updateData: function(data) {
-        //update title
-        this.BillDetailsView.query('#billTitle')[0].update({
-            title: data.bill_title
-        });
-
-        //update billMakersStore
-        var billMakers = {
-            founders: data.proposing_mks,
-            joined: data.joining_mks
-        }
-
-        billMakers.founders.forEach(function (member) {
-            member.MemberType = 'founder';
-        });
-
-        billMakers.joined.forEach(function (member) {
-            member.MemberType = 'joined';
-        });
-
-        OKnesset.billMakersStore.loadData(billMakers.founders);
-        OKnesset.billMakersStore.add(billMakers.joined);
-
-        //update Content
-        var explanation  = "";
-        var expalantions = [];
-        var pdf = -1;
-        var pdfs = [];
-        if (data.proposals.private_proposals[0] != null) {
-             expalantions.push(data.proposals.private_proposals[0].explanation);
-
-             if (data.proposals.private_proposals[0].source_url.match(/\.pdf/))
-                pdfs.push(data.proposals.private_proposals[0].source_url);
-         }
-         else if (data.proposals.gov_proposal.explanation != null) {
-             expalantions.push(data.proposals.gov_proposal.explanation);
-
-             if (data.proposals.gov_proposal.source_url.match(/\.pdf/))
-                pdfs.push(data.proposals.gov_proposal.source_url);
-
-         }
-         else if (data.proposals.knesset_proposal.explanation != null) {
-            expalantions.push(data.proposals.knesset_proposal.explanation);
-
-             if (data.proposals.knesset_proposal.source_url.match(/\.pdf$/))
-                pdfs.push(data.proposals.knesset_proposal.source_url);
-         }
-
-         expalantions.forEach(function(e){
-            if (e != "")
-                explanation = e;
-         });
-
-         pdfs.forEach(function(e){
-            if (e != "")
-                pdf = e;
-         });
-
-        this.BillDetailsView.query('#billContent')[0].update({
-            explanation: explanation
-        });
-
-        //update state & Link to PDF
-        this.BillDetailsView.query('#billStage')[0].update({
-            stage_text: data.stage_text,
-            stage_date:data.stage_date,
-            linkToPdf: pdf
-        });
+		if (BillDetailsController.BillDetailsView.scroller) {
+			BillDetailsController.BillDetailsView.scroller.scrollTo({
+				x: 0,
+				y: 0
+			});
+		}
+	},
 
 
-    },
-        _gotoMember: function(record){
-        OKnesset.app.controllers.navigation.dispatchPanel('Member/Index/' + record.data.id, this.historyUrl);
-    }
+	_init: function(elementsToHIDE){
+
+		this.BillDetailsView.query('#billTitle')[0].update({
+			title: OKnesset.strings.LoadingPlsWait
+		});
+
+		elementsToHIDE.forEach(function(e){
+			e.hide();
+		});
+	},
+	_refresh: function(elementsToSHOW){
+
+		elementsToSHOW.forEach(function(e){
+			e.show();
+		});
+	},
+
+	updateData: function(data) {
+		//update title
+		this.BillDetailsView.query('#billTitle')[0].update({
+			title: data.bill_title
+		});
+
+		//update billMakersStore
+		var billMakers = {
+			founders: data.proposing_mks,
+			joined: data.joining_mks
+		};
+
+		billMakers.founders.forEach(function (member) {
+			member.MemberType = 'founder';
+		});
+
+		billMakers.joined.forEach(function (member) {
+			member.MemberType = 'joined';
+		});
+
+		OKnesset.billMakersStore.loadData(billMakers.founders);
+		OKnesset.billMakersStore.add(billMakers.joined);
+
+		//update Content
+		var explanation  = "",
+			expalantions = [],
+			pdf = -1,
+			pdfs = [];
+		if (data.proposals.private_proposals[0]) {
+			expalantions.push(data.proposals.private_proposals[0].explanation);
+
+			if (data.proposals.private_proposals[0].source_url.match(/\.pdf/)) {
+				pdfs.push(data.proposals.private_proposals[0].source_url);
+			}
+		} else if (data.proposals.gov_proposal.explanation) {
+			expalantions.push(data.proposals.gov_proposal.explanation);
+
+			if (data.proposals.gov_proposal.source_url.match(/\.pdf/)){
+				pdfs.push(data.proposals.gov_proposal.source_url);
+			}
+		} else if (data.proposals.knesset_proposal.explanation) {
+			expalantions.push(data.proposals.knesset_proposal.explanation);
+
+			if (data.proposals.knesset_proposal.source_url.match(/\.pdf$/)) {
+				pdfs.push(data.proposals.knesset_proposal.source_url);
+			}
+		}
+
+		expalantions.forEach(function(e){
+			if (e) {
+				explanation = e;
+			}
+		});
+
+		pdfs.forEach(function(e){
+			if (e) {
+				pdf = e;
+			}
+		});
+
+		this.BillDetailsView.query('#billContent')[0].update({
+			explanation: explanation
+		});
+
+		//update state & Link to PDF
+		this.BillDetailsView.query('#billStage')[0].update({
+			stage_text: data.stage_text,
+			stage_date:data.stage_date,
+			linkToPdf: pdf
+		});
+
+
+	},
+		_gotoMember: function(record){
+		OKnesset.app.controllers.navigation.dispatchPanel('Member/Index/' + record.data.id, this.historyUrl);
+	}
 
 });
