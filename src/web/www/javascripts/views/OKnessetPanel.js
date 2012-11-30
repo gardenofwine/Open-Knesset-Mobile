3f3c6034293cb293c775e306462022adc0ab82aa
gardenofwine <gardenofwine@gmail.com>
2012-11-30 11:30:54 +0200

    fix "loading please wait" dialog

--- src/web/www/javascripts/views/OKnessetPanel.js
+++ src/web/www/javascripts/views/OKnessetPanel.js
@@ -14,12 +14,30 @@ OKnesset.Panel = Ext.extend(Ext.Panel, {
 		this.items.splice(0, 0, this.loadingPanel);
 		OKnesset.Panel.superclass.initComponent.call(this,arguments);
 	},
+	listeners:{
+		show : {
+			fn : function(that){
+				if (that._showLoadingOnShow){
+					that.loadingPanel.show();
+				}
+			}
+		}
+
+	},
 
 	showLoading: function(show) {
 		if (show){
-			this.loadingPanel.show();
+			if (this.isVisible()){
+			// var that = this;
+			// window.setTimeout(function(){that.loadingPanel.show();}, 10);
+				this.loadingPanel.show();
+				this._showLoadingOnShow = false;
+			} else {
+				this._showLoadingOnShow = true;
+			}
 		} else {
 			this.loadingPanel.hide();
+			this._showLoadingOnShow = false;
 		}
 	}
 });
