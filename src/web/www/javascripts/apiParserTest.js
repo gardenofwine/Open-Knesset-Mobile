39f444f606d4632fa5a28b96c680bd693250885f
gardenofwine <gardenofwine@gmail.com>
2012-12-30 20:42:12 +0200

    apiParserTest logs api call count

--- src/web/www/javascripts/apiParserTest.js
+++ src/web/www/javascripts/apiParserTest.js
@@ -2,48 +2,60 @@ OKnesset = ((typeof OKnesset !== 'undefined')?OKnesset :{});
 OKnesset.debug = true;
 
 function testAPIParser(){
-
 	var apiCalls = Object.keys(OKnessetAPIMapping);
+	var apiCallCounter = 0;
+	var apiDataObjectParameters = [];
+
 	for (var i = 0; i < apiCalls.length ; i++) {
-		try {
-			var baseAPIDataObjectParameter = {
-				apiKey : apiCalls[i],
-				success: function(){},
-				failure: function(){}
-			}
-			var apiDataObjectParameters = [];
-			console.log(apiCalls[i]);
-			if (typeof OKnessetAPIMapping[apiCalls[i]].sampleUrl !== 'undefined'){
-				for (var j = 0; j < OKnessetAPIMapping[apiCalls[i]].sampleUrl.length; j++) {
-					apiDataObjectParameters.push(
-						Ext.apply(
-							{
-								urlOptions:OKnessetAPIMapping[apiCalls[i]].sampleUrl[j]
-							}, 
-							baseAPIDataObjectParameter));
-				};
-			} else if (typeof OKnessetAPIMapping[apiCalls[i]].sampleParameters !== 'undefined'){
-				// assumig an api call does not require both a urlParams and a get param
-				for (var j = 0; j < OKnessetAPIMapping[apiCalls[i]].sampleParameters.length; j++) {
-					apiDataObjectParameters.push(
-						Ext.apply(
-							{
-								parameterOptions:OKnessetAPIMapping[apiCalls[i]].sampleParameters[j]
-							}, 
-							baseAPIDataObjectParameter));
-				};
-			} else {
-				apiDataObjectParameters.push(baseAPIDataObjectParameter);
-			}
-
-
-			// make the api call
-			for (var j = 0; j < apiDataObjectParameters.length; j++) {
-				getAPIData(apiDataObjectParameters[j]);
+		var theApiCall = apiCalls[i] 
+		var baseAPIDataObjectParameter = (function(apiCall){
+			return {
+				apiKey : theApiCall,
+				forceLoad : true,
+				success: function(data){
+					console.log("" + apiCall + " success. " + apiCallCounter + " more to go");
+					apiCallCounter --;
+				},
+				failure: function(){
+					console.error("" + apiCall + " failure. " + apiCallCounter + " more to go");
+					apiCallCounter --;
+				}
 			};
-			 
-		} catch (e) {
-			console.log("exception")
+		})(theApiCall);
+
+		if (typeof OKnessetAPIMapping[apiCalls[i]].sampleUrl !== 'undefined'){
+			for (var j = 0; j < OKnessetAPIMapping[apiCalls[i]].sampleUrl.length; j++) {
+				apiDataObjectParameters.push(
+					Ext.apply(
+						{
+							urlOptions:OKnessetAPIMapping[apiCalls[i]].sampleUrl[j]
+						}, 
+						baseAPIDataObjectParameter));
+			};
+		} else if (typeof OKnessetAPIMapping[apiCalls[i]].sampleParameters !== 'undefined'){
+			// assumig an api call does not require both a urlParams and a get param
+			for (var j = 0; j < OKnessetAPIMapping[apiCalls[i]].sampleParameters.length; j++) {
+				apiDataObjectParameters.push(
+					Ext.apply(
+						{
+							parameterOptions:OKnessetAPIMapping[apiCalls[i]].sampleParameters[j]
+						}, 
+						baseAPIDataObjectParameter));
+			};
+		} else {
+			apiDataObjectParameters.push(baseAPIDataObjectParameter);
 		}
-	};
+	}
+
+	try {
+
+		apiCallCounter = apiDataObjectParameters.length;
+		// make the api call
+		for (var j = 0; j < apiDataObjectParameters.length; j++) {
+			getAPIData(apiDataObjectParameters[j]);
+		};
+			 
+	} catch (e) {
+		console.error("exception")
+	}
 }
