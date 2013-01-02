b3f6fcf6969f7f97d151e06452be2525c2f23592
gardenofwine <gardenofwine@gmail.com>
2013-01-02 11:26:50 +0200

    elections -  electionData now holds party data only

--- static/V3.0/elections/electionData.js
+++ static/V3.0/elections/electionData.js
@@ -1,253 +1,251 @@
-Ext.util.JSONP.callback({
-	"parties": [
-		{
-			"id": 1,
-			"name": "אור",
-			"letters": "ני",
-			"website": "http://www.orr.org.il/",
-			"manifest_url": "http://www.orr.org.il/index.php?option=com_content&view=article&id=1&Itemid=2"
-		},
-		{
-			"id": 2,
-			"party_id": 4,
-			"name": "יהדות התורה והשבת אגודת ישראל - דגל התורה",
-			"letters": "ג",
-			"website": "http://he.wikipedia.org/wiki/%D7%99%D7%94%D7%93%D7%95%D7%AA_%D7%94%D7%AA%D7%95%D7%A8%D7%94",
-			"manifest_url": ""
-		},
-		{
-			"id": 3,
-			"name": "אחים אנחנו",
-			"letters": "פנ",
-			"website": "http://www.ahim-anahnu.org/",
-			"manifest_url": "http://www.ahim-anahnu.org/viewStaticPage.aspx?pageID=4"
-		},
-		{
-			"id": 4,
-			"name": "יש עתיד בראשות יאיר לפיד",
-			"letters": "פה",
-			"website": "http://yeshatid.org.il/",
-			"manifest_url": "http://yeshatid.org.il/%D7%A2%D7%9C-%D7%94%D7%9E%D7%A4%D7%9C%D7%92%D7%94-2/%D7%9E%D7%A0%D7%95%D7%A4%D7%99-%D7%94%D7%A9%D7%99%D7%A0%D7%95%D7%99/        http://yeshatid.org.il/"
-		},
-		{
-			"id": 5,
-			"name": "ארץ חדשה - החזית להפלת שלטון ההון",
-			"letters": "ז",
-			"website": "http://eretzchadasha.com/",
-			"manifest_url": "http://eretzchadasha.com/who-we-are"
-		},
-		{
-			"id": 6,
-			"name": "כולנו חברים - ננח",
-			"letters": "פץ",
-			"website": "http://www.nanach.co.il",
-			"manifest_url": "http://www.nanach.co.il/?page_id=18"
-		},
-		{
-			"id": 7,
-			"party_id": 9,
-			"name": "בל”ד - אלתג`מוע אלווטני אלדמוקרטי",
-			"letters": "ד",
-			"website": "http://www.tajamoa.org/",
-			"manifest_url": "http://www.tajamoa.org/?mod=arch&ID=22"
-		},
-		{
-			"id": 8,
-			"name": "כח להשפיע בהנחייה / בהכוונת הרב אמנון יצחק",
-			"letters": "פז",
-			"website": "http://he.wikipedia.org/wiki/%D7%9B%D7%97_%D7%9C%D7%94%D7%A9%D7%A4%D7%99%D7%A2",
-			"manifest_url": ""
-		},
-		{
-			"id": 9,
-			"name": "ברית עולם לגאולת ישראל",
-			"letters": "נ",
-			"website": "http://www.britolam.org.il/",
-			"manifest_url": "http://www.britolam.org.il/187853/%D7%A2%D7%99%D7%A7%D7%A8%D7%99-%D7%94%D7%9E%D7%A6%D7%A2"
-		},
-		{
-			"id": 10,
-			"name": "מורשת אבות",
-			"letters": "הי",
-			"website": "http://he.wikipedia.org/wiki/%D7%9E%D7%95%D7%A8%D7%A9%D7%AA_%D7%90%D7%91%D7%95%D7%AA",
-			"manifest_url": "http://www.facebook.com/pages/%D7%9E%D7%95%D7%A8%D7%A9%D7%AA-%D7%90%D7%91%D7%95%D7%AA-%D7%90%D7%99%D7%9C%D7%9F-%D7%9E%D7%A9%D7%99%D7%97%D7%90/553431928004275?ref=stream"
-		},
-		{
-			"id": 11,
-			"name": "דור בוני הארץ בראשות אפרים לפיד",
-			"letters": "זך",
-			"website": "http://www.gimlaim.org.il/BRPortal/br/P100.jsp",
-			"manifest_url": "http://www.gimlaim.org.il/BRPortal/br/P103.jsp?cat=29224"
-		},
-		{
-			"id": 12,
-			"name": "מפלגת כלכלה בראשות יוליה שמאלוב ברקוביץ",
-			"letters": "פי",
-			"website": "http://he.wikipedia.org/wiki/%D7%9B%D7%9C%D7%9B%D7%9C%D7%94_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
-			"manifest_url": "http://www.facebook.com/media/set/?set=a.234353946669627.43722.228132433958445&type=3"
-		},
-		{
-			"id": 13,
-			"name": "דע\"ם - מפלגת פועלים",
-			"letters": "ק",
-			"website": "http://www.daam.org.il/",
-			"manifest_url": "http://heb.daam.org.il/%D7%9E%D7%A6%D7%A2-%D7%93%D7%A2%D7%9D/"
-		},
-		{
-			"id": 14,
-			"party_id": 11,
-			"name": "מרצ - השמאל של ישראל",
-			"letters": "מרצ",
-			"website": "http://meretz.org.il/",
-			"manifest_url": "http://meretz.org.il/%D7%9E%D7%A6%D7%A2-%D7%94%D7%9E%D7%A4%D7%9C%D7%92%D7%94/"
-		},
-		{
-			"id": 15,
-			"party_id": 12,
-			"name": "הבית היהודי בראשות נפתלי בנט - מיסודם של האיחוד הלאומי מפד”ל החדשה",
-			"letters": "טב",
-			"website": "http://www.israelim.org.il/",
-			"manifest_url": "http://www.israelim.org.il/our-principles/"
-		},
-		{
-			"id": 16,
-			"name": "מתקדמת ליברלית - דמוקרטית",
-			"letters": "נק",
-			"website": "http://he.wikipedia.org/wiki/%D7%9C%D7%99%D7%93%D7%A8_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
-			"manifest_url": ""
-		},
-		{
-			"id": 17,
-			"name": "הירוקים והצעירים לעתיד ירוק בישראל",
-			"letters": "רק",
-			"website": "http://www.green-party.co.il/Election2013/",
-			"manifest_url": "http://www.green-party.co.il/Election2013/matza"
-		},
-		{
-			"id": 18,
-			"name": "נצח",
-			"letters": "פך",
-			"website": "http://he.wikipedia.org/wiki/%D7%A0%D7%A6%D7%97_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
-			"manifest_url": ""
-		},
-		{
-			"id": 19,
-			"name": "הישראלים",
-			"letters": "יק",
-			"website": "http://he.wikipedia.org/wiki/%D7%94%D7%99%D7%A9%D7%A8%D7%90%D7%9C%D7%99%D7%9D_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
-			"manifest_url": ""
-		},
-		{
-			"id": 20,
-			"party_id": 1,
-			"name": "עוצמה לישראל בראשות אריה אלדד ומיכאל בן ארי",
-			"letters": "נץ",
-			"website": "http://otzmaleisrael.co.il/",
-			"manifest_url": "http://otzmaleisrael.co.il/main-platform/"
-		},
-		{
-			"id": 21,
-			"party_id": 2,
-			"name": "הליכוד ישראל ביתנו בהנהגת בנימין נתניהו לראשות הממשלה",
-			"letters": "מחל",
-			"website": "https://www.likud.org.il/",
-			"manifest_url": "המצע של הליכוד עדיין בתהליך כתיבה (http://www.ynet.co.il/articles/0,7340,L-4319455,00.html). המצע של ישראל ביתנו: http://www.beytenu.org.il/category/%D7%9E%D7%A6%D7%A2/"
-		},
-		{
-			"id": 22,
-			"name": "עלה ירוק - הרשימה הליברלית",
-			"letters": "קנ",
-			"website": "http://aleyarok.org.il/",
-			"manifest_url": "http://aleyarok.org.il/agenda/"
-		},
-		{
-			"id": 23,
-			"party_id": 21,
-			"name": "העבודה בראשות שלי יחימוביץ",
-			"letters": "אמת",
-			"website": "http://www.havoda.org.il/Web/Default.aspx",
-			"manifest_url": "http://havoda.org.il/Items/05463/%D7%9E%D7%A6%D7%A2%20%D7%94%D7%9E%D7%A4%D7%9C%D7%92%D7%94%20%D7%9C%D7%9B%D7%A0%D7%A1%D7%AA%20%D7%94-19.pdf"
-		},
-		{
-			"id": 24,
-			"name": "עם שלם - בראשות הרב חיים אמסלם",
-			"letters": "ץ",
-			"website": "http://amshalem.org/",
-			"manifest_url": "http://amshalem.org/?page_id=21"
-		},
-		{
-			"id": 25,
-			"name": "הפיראטים",
-			"letters": "פ",
-			"website": "http://piratim.org/",
-			"manifest_url": "http://www.piratim.org/wiki/doku.php?id=%D7%9E%D7%A6%D7%A2"
-		},
-		{
-			"id": 26,
-			"name": "עתיד אחד – נלחמים בסם החדש",
-			"letters": "זה",
-			"website": "http://www.atid1.org.il/",
-			"manifest_url": ""
-		},
-		{
-			"id": 34,
-			"name": "התנועה בראשות ציפי לבני",
-			"letters": "צפ",
-			"website": "http://he.wikipedia.org/wiki/%D7%94%D7%AA%D7%A0%D7%95%D7%A2%D7%94",
-			"manifest_url": ""
-		},
-		{
-			"id": 27,
-			"name": "צדק חברתי בראשות גד הרן",
-			"letters": "צק",
-			"website": "http://www.tzedekhev.com/",
-			"manifest_url": "http://www.tzedekhev.com/#!our_team/cqn6"
-		},
-		{
-			"id": 28,
-			"name": "אלאמל לתג`ייר - התקווה לשינוי",
-			"letters": "הק",
-			"website": "http://hope-for-change.org/index_h.php",
-			"manifest_url": "http://hope-for-change.org/about_h.php"
-		},
-		{
-			"id": 29,
-			"party_id": 6,
-			"name": "קדימה בראשות שאול מופז",
-			"letters": "כן",
-			"website": "http://he.wikipedia.org/wiki/%D7%A7%D7%93%D7%99%D7%9E%D7%94",
-			"manifest_url": "http://www.facebook.com/kadimaparty/app_376885859069497"
-		},
-		{
-			"id": 30,
-			"party_id": 8,
-			"name": "חד”ש – חזית דמוקרטית לשלום ושוויון (המפלגה הקומוניסטית הישראלית – תראבוט וחוגי ציבור ערבים ויהודים)",
-			"letters": "ו",
-			"website": "http://hadash.org.il/",
-			"manifest_url": "http://hadash.org.il/matzahadash/"
-		},
-		{
-			"id": 31,
-			"party_id": 10,
-			"name": "רע”מ - תע”ל - מד”ע",
-			"letters": "עם",
-			"website": "http://www.a-m-c.org/",
-			"manifest_url": "http://www.a-m-c.org/page.asp?id=566"
-		},
-		{
-			"id": 32,
-			"name": "חיים בכבוד",
-			"letters": "הפ",
-			"website": "https://www.facebook.com/pages/%D7%9E%D7%A4%D7%9C%D7%92%D7%AA-%D7%97%D7%99%D7%99%D7%9D-%D7%91%D7%9B%D7%91%D7%95%D7%93/438796402841642",
-			"manifest_url": ""
-		},
-		{
-			"id": 33,
-			"party‫_‬id": 7,
-			"name": "שס התאחדות הספרדית העולמית שומרי תורה",
-			"letters": "שס",
-			"website": "shas.org.il",
-			"manifest_url": "http://shas.org.il/Web/He/About/Platform/Default.aspx"
-		}
-	]
-});
+Ext.util.JSONP.callback([
+	{
+		"id": 1,
+		"name": "אור",
+		"letters": "ני",
+		"website": "http://www.orr.org.il/",
+		"manifest_url": "http://www.orr.org.il/index.php?option=com_content&view=article&id=1&Itemid=2"
+	},
+	{
+		"id": 2,
+		"party_id": 4,
+		"name": "יהדות התורה והשבת אגודת ישראל - דגל התורה",
+		"letters": "ג",
+		"website": "http://he.wikipedia.org/wiki/%D7%99%D7%94%D7%93%D7%95%D7%AA_%D7%94%D7%AA%D7%95%D7%A8%D7%94",
+		"manifest_url": ""
+	},
+	{
+		"id": 3,
+		"name": "אחים אנחנו",
+		"letters": "פנ",
+		"website": "http://www.ahim-anahnu.org/",
+		"manifest_url": "http://www.ahim-anahnu.org/viewStaticPage.aspx?pageID=4"
+	},
+	{
+		"id": 4,
+		"name": "יש עתיד בראשות יאיר לפיד",
+		"letters": "פה",
+		"website": "http://yeshatid.org.il/",
+		"manifest_url": "http://yeshatid.org.il/%D7%A2%D7%9C-%D7%94%D7%9E%D7%A4%D7%9C%D7%92%D7%94-2/%D7%9E%D7%A0%D7%95%D7%A4%D7%99-%D7%94%D7%A9%D7%99%D7%A0%D7%95%D7%99/        http://yeshatid.org.il/"
+	},
+	{
+		"id": 5,
+		"name": "ארץ חדשה - החזית להפלת שלטון ההון",
+		"letters": "ז",
+		"website": "http://eretzchadasha.com/",
+		"manifest_url": "http://eretzchadasha.com/who-we-are"
+	},
+	{
+		"id": 6,
+		"name": "כולנו חברים - ננח",
+		"letters": "פץ",
+		"website": "http://www.nanach.co.il",
+		"manifest_url": "http://www.nanach.co.il/?page_id=18"
+	},
+	{
+		"id": 7,
+		"party_id": 9,
+		"name": "בל”ד - אלתג`מוע אלווטני אלדמוקרטי",
+		"letters": "ד",
+		"website": "http://www.tajamoa.org/",
+		"manifest_url": "http://www.tajamoa.org/?mod=arch&ID=22"
+	},
+	{
+		"id": 8,
+		"name": "כח להשפיע בהנחייה / בהכוונת הרב אמנון יצחק",
+		"letters": "פז",
+		"website": "http://he.wikipedia.org/wiki/%D7%9B%D7%97_%D7%9C%D7%94%D7%A9%D7%A4%D7%99%D7%A2",
+		"manifest_url": ""
+	},
+	{
+		"id": 9,
+		"name": "ברית עולם לגאולת ישראל",
+		"letters": "נ",
+		"website": "http://www.britolam.org.il/",
+		"manifest_url": "http://www.britolam.org.il/187853/%D7%A2%D7%99%D7%A7%D7%A8%D7%99-%D7%94%D7%9E%D7%A6%D7%A2"
+	},
+	{
+		"id": 10,
+		"name": "מורשת אבות",
+		"letters": "הי",
+		"website": "http://he.wikipedia.org/wiki/%D7%9E%D7%95%D7%A8%D7%A9%D7%AA_%D7%90%D7%91%D7%95%D7%AA",
+		"manifest_url": "http://www.facebook.com/pages/%D7%9E%D7%95%D7%A8%D7%A9%D7%AA-%D7%90%D7%91%D7%95%D7%AA-%D7%90%D7%99%D7%9C%D7%9F-%D7%9E%D7%A9%D7%99%D7%97%D7%90/553431928004275?ref=stream"
+	},
+	{
+		"id": 11,
+		"name": "דור בוני הארץ בראשות אפרים לפיד",
+		"letters": "זך",
+		"website": "http://www.gimlaim.org.il/BRPortal/br/P100.jsp",
+		"manifest_url": "http://www.gimlaim.org.il/BRPortal/br/P103.jsp?cat=29224"
+	},
+	{
+		"id": 12,
+		"name": "מפלגת כלכלה בראשות יוליה שמאלוב ברקוביץ",
+		"letters": "פי",
+		"website": "http://he.wikipedia.org/wiki/%D7%9B%D7%9C%D7%9B%D7%9C%D7%94_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
+		"manifest_url": "http://www.facebook.com/media/set/?set=a.234353946669627.43722.228132433958445&type=3"
+	},
+	{
+		"id": 13,
+		"name": "דע\"ם - מפלגת פועלים",
+		"letters": "ק",
+		"website": "http://www.daam.org.il/",
+		"manifest_url": "http://heb.daam.org.il/%D7%9E%D7%A6%D7%A2-%D7%93%D7%A2%D7%9D/"
+	},
+	{
+		"id": 14,
+		"party_id": 11,
+		"name": "מרצ - השמאל של ישראל",
+		"letters": "מרצ",
+		"website": "http://meretz.org.il/",
+		"manifest_url": "http://meretz.org.il/%D7%9E%D7%A6%D7%A2-%D7%94%D7%9E%D7%A4%D7%9C%D7%92%D7%94/"
+	},
+	{
+		"id": 15,
+		"party_id": 12,
+		"name": "הבית היהודי בראשות נפתלי בנט - מיסודם של האיחוד הלאומי מפד”ל החדשה",
+		"letters": "טב",
+		"website": "http://www.israelim.org.il/",
+		"manifest_url": "http://www.israelim.org.il/our-principles/"
+	},
+	{
+		"id": 16,
+		"name": "מתקדמת ליברלית - דמוקרטית",
+		"letters": "נק",
+		"website": "http://he.wikipedia.org/wiki/%D7%9C%D7%99%D7%93%D7%A8_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
+		"manifest_url": ""
+	},
+	{
+		"id": 17,
+		"name": "הירוקים והצעירים לעתיד ירוק בישראל",
+		"letters": "רק",
+		"website": "http://www.green-party.co.il/Election2013/",
+		"manifest_url": "http://www.green-party.co.il/Election2013/matza"
+	},
+	{
+		"id": 18,
+		"name": "נצח",
+		"letters": "פך",
+		"website": "http://he.wikipedia.org/wiki/%D7%A0%D7%A6%D7%97_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
+		"manifest_url": ""
+	},
+	{
+		"id": 19,
+		"name": "הישראלים",
+		"letters": "יק",
+		"website": "http://he.wikipedia.org/wiki/%D7%94%D7%99%D7%A9%D7%A8%D7%90%D7%9C%D7%99%D7%9D_(%D7%9E%D7%A4%D7%9C%D7%92%D7%94)",
+		"manifest_url": ""
+	},
+	{
+		"id": 20,
+		"party_id": 1,
+		"name": "עוצמה לישראל בראשות אריה אלדד ומיכאל בן ארי",
+		"letters": "נץ",
+		"website": "http://otzmaleisrael.co.il/",
+		"manifest_url": "http://otzmaleisrael.co.il/main-platform/"
+	},
+	{
+		"id": 21,
+		"party_id": 2,
+		"name": "הליכוד ישראל ביתנו בהנהגת בנימין נתניהו לראשות הממשלה",
+		"letters": "מחל",
+		"website": "https://www.likud.org.il/",
+		"manifest_url": "המצע של הליכוד עדיין בתהליך כתיבה (http://www.ynet.co.il/articles/0,7340,L-4319455,00.html). המצע של ישראל ביתנו: http://www.beytenu.org.il/category/%D7%9E%D7%A6%D7%A2/"
+	},
+	{
+		"id": 22,
+		"name": "עלה ירוק - הרשימה הליברלית",
+		"letters": "קנ",
+		"website": "http://aleyarok.org.il/",
+		"manifest_url": "http://aleyarok.org.il/agenda/"
+	},
+	{
+		"id": 23,
+		"party_id": 21,
+		"name": "העבודה בראשות שלי יחימוביץ",
+		"letters": "אמת",
+		"website": "http://www.havoda.org.il/Web/Default.aspx",
+		"manifest_url": "http://havoda.org.il/Items/05463/%D7%9E%D7%A6%D7%A2%20%D7%94%D7%9E%D7%A4%D7%9C%D7%92%D7%94%20%D7%9C%D7%9B%D7%A0%D7%A1%D7%AA%20%D7%94-19.pdf"
+	},
+	{
+		"id": 24,
+		"name": "עם שלם - בראשות הרב חיים אמסלם",
+		"letters": "ץ",
+		"website": "http://amshalem.org/",
+		"manifest_url": "http://amshalem.org/?page_id=21"
+	},
+	{
+		"id": 25,
+		"name": "הפיראטים",
+		"letters": "פ",
+		"website": "http://piratim.org/",
+		"manifest_url": "http://www.piratim.org/wiki/doku.php?id=%D7%9E%D7%A6%D7%A2"
+	},
+	{
+		"id": 26,
+		"name": "עתיד אחד – נלחמים בסם החדש",
+		"letters": "זה",
+		"website": "http://www.atid1.org.il/",
+		"manifest_url": ""
+	},
+	{
+		"id": 34,
+		"name": "התנועה בראשות ציפי לבני",
+		"letters": "צפ",
+		"website": "http://he.wikipedia.org/wiki/%D7%94%D7%AA%D7%A0%D7%95%D7%A2%D7%94",
+		"manifest_url": ""
+	},
+	{
+		"id": 27,
+		"name": "צדק חברתי בראשות גד הרן",
+		"letters": "צק",
+		"website": "http://www.tzedekhev.com/",
+		"manifest_url": "http://www.tzedekhev.com/#!our_team/cqn6"
+	},
+	{
+		"id": 28,
+		"name": "אלאמל לתג`ייר - התקווה לשינוי",
+		"letters": "הק",
+		"website": "http://hope-for-change.org/index_h.php",
+		"manifest_url": "http://hope-for-change.org/about_h.php"
+	},
+	{
+		"id": 29,
+		"party_id": 6,
+		"name": "קדימה בראשות שאול מופז",
+		"letters": "כן",
+		"website": "http://he.wikipedia.org/wiki/%D7%A7%D7%93%D7%99%D7%9E%D7%94",
+		"manifest_url": "http://www.facebook.com/kadimaparty/app_376885859069497"
+	},
+	{
+		"id": 30,
+		"party_id": 8,
+		"name": "חד”ש – חזית דמוקרטית לשלום ושוויון (המפלגה הקומוניסטית הישראלית – תראבוט וחוגי ציבור ערבים ויהודים)",
+		"letters": "ו",
+		"website": "http://hadash.org.il/",
+		"manifest_url": "http://hadash.org.il/matzahadash/"
+	},
+	{
+		"id": 31,
+		"party_id": 10,
+		"name": "רע”מ - תע”ל - מד”ע",
+		"letters": "עם",
+		"website": "http://www.a-m-c.org/",
+		"manifest_url": "http://www.a-m-c.org/page.asp?id=566"
+	},
+	{
+		"id": 32,
+		"name": "חיים בכבוד",
+		"letters": "הפ",
+		"website": "https://www.facebook.com/pages/%D7%9E%D7%A4%D7%9C%D7%92%D7%AA-%D7%97%D7%99%D7%99%D7%9D-%D7%91%D7%9B%D7%91%D7%95%D7%93/438796402841642",
+		"manifest_url": ""
+	},
+	{
+		"id": 33,
+		"party‫_‬id": 7,
+		"name": "שס התאחדות הספרדית העולמית שומרי תורה",
+		"letters": "שס",
+		"website": "shas.org.il",
+		"manifest_url": "http://shas.org.il/Web/He/About/Platform/Default.aspx"
+	}
+]);
